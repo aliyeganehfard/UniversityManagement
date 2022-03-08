1428295eb038816b7fdf428b6254c4dbb3913e70
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-03-08 16:13:40 +0330

    add singleTonConnection test class

---
+++ src/test/java/model/util/SingleTonConnectionTest.java
@@ -0,0 +1,13 @@
+package model.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SingleTonConnectionTest {
+
+    @Test
+    public void testConnection(){
+        assertDoesNotThrow(SingleTonConnection::getInstance);
+    }
+}
