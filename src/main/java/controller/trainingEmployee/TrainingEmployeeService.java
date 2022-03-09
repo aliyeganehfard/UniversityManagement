f8304f645b3db6decf301e4a3a04939cbf9ec525
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-03-09 21:34:08 +0330

    add TrainingEmployeeService interface

---
+++ src/main/java/controller/trainingEmployee/TrainingEmployeeService.java
@@ -0,0 +1,6 @@
+package controller.trainingEmployee;
+
+import model.repository.trainingEmployee.TrainingEmployeeRepository;
+
+public interface TrainingEmployeeService extends TrainingEmployeeRepository {
+}
