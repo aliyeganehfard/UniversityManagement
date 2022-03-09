1d06147587253e503d84d315fedc4a0d8743253a
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-03-09 20:52:55 +0330

    add ScoreRepositoryImpl class

---
+++ src/main/java/model/repository/score/ScoreRepositoryImpl.java
@@ -0,0 +1,8 @@
+package model.repository.score;
+
+import model.entity.Score;
+import model.repository.Repository;
+import model.repository.RepositoryImpl;
+
+public class ScoreRepositoryImpl extends RepositoryImpl<Score,Integer> implements ScoreRepository {
+}
