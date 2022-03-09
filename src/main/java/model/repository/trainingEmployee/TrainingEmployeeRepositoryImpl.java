a1797a5d414836b5ae36b2872cf6619b2b28730a
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-03-10 00:54:45 +0330

    fix add TrainingEmployeeRepositoryImplTest class

--- src/main/java/model/repository/trainingEmployee/TrainingEmployeeRepositoryImpl.java
+++ src/main/java/model/repository/trainingEmployee/TrainingEmployeeRepositoryImpl.java
@@ -1,7 +1,31 @@
 package model.repository.trainingEmployee;
 
 import model.entity.TrainingEmployee;
+import model.repository.LoginBase;
 import model.repository.RepositoryImpl;
+import model.util.SingleTonConnection;
 
-public class TrainingEmployeeRepositoryImpl extends RepositoryImpl<TrainingEmployee,Integer> implements TrainingEmployeeRepository {
+public class TrainingEmployeeRepositoryImpl extends RepositoryImpl<TrainingEmployee,Integer> implements  TrainingEmployeeRepository {
+public TrainingEmployee login(String userName , String password){
+        try(var session = getSessionFactory().openSession()){
+            var transaction = session.getTransaction();
+            try{
+                var criteriaBuilder = session.getCriteriaBuilder();
+                var criteriaQuery = criteriaBuilder.createQuery(TrainingEmployee.class);
+                var root = criteriaQuery.from(TrainingEmployee.class);
+
+                var userNameEqual = criteriaBuilder.equal(root.get("userName"),userName);
+                var passwordEqual = criteriaBuilder.equal(root.get("password"),password);
+
+                var query = criteriaQuery
+                        .select(root)
+                        .where(criteriaBuilder.and(userNameEqual,passwordEqual));
+                return session.createQuery(query).getSingleResult();
+            }catch (Exception e){
+                transaction.rollback();
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
 }
