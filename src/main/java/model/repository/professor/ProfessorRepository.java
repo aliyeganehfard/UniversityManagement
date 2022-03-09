6b3bf630d0993494fe04f7697c8ffd4d24db2c53
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-03-10 00:52:45 +0330

    fix add ProfessorRepository interface

--- src/main/java/model/repository/professor/ProfessorRepository.java
+++ src/main/java/model/repository/professor/ProfessorRepository.java
@@ -1,9 +1,11 @@
 package model.repository.professor;
 
+import model.entity.Professor;
 import model.entity.Score;
+import model.repository.LoginBase;
 
 import java.util.List;
 
-public interface ProfessorRepository {
+public interface ProfessorRepository extends LoginBase<Professor> {
     List<Score> getProfessorCourse(Integer id);
 }
