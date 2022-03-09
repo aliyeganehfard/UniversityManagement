76a3301959f2037ffe248c792977734d3680a581
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-03-09 20:50:18 +0330

    add TrainingEmployeeRepositoryImpl class

---
+++ src/main/java/model/repository/trainingEmployee/TrainingEmployeeRepositoryImpl.java
@@ -0,0 +1,7 @@
+package model.repository.trainingEmployee;
+
+import model.entity.TrainingEmployee;
+import model.repository.RepositoryImpl;
+
+public class TrainingEmployeeRepositoryImpl extends RepositoryImpl<TrainingEmployee,Integer> implements TrainingEmployeeRepository {
+}
