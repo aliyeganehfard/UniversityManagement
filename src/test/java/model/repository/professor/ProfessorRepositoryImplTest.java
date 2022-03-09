aef56910fa5707a64dfae3d8564eb33dcdc294fb
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-03-10 00:53:09 +0330

    fix add ProfessorRepositoryImplTest test

--- src/test/java/model/repository/professor/ProfessorRepositoryImplTest.java
+++ src/test/java/model/repository/professor/ProfessorRepositoryImplTest.java
@@ -1,8 +1,8 @@
 package model.repository.professor;
 
-import model.entity.College;
-import model.entity.Professor;
+import model.entity.*;
 import model.repository.college.CollegeRepositoryImpl;
+import model.repository.score.ScoreRepositoryImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,7 +25,7 @@ class ProfessorRepositoryImplTest {
     void save() {
 //        arrange
         var college = new College(null, "azad", "lar");
-        var professor = new Professor(null,"ali","b", "a", college);
+        var professor = new Professor(null, "ali", "1245", "123", "b", "a", college);
 //        act
         collegeRepository.save(college);
         professorRepository.save(professor);
@@ -38,41 +38,41 @@ class ProfessorRepositoryImplTest {
     }
 
     @Test
-    void update(){
+    void update() {
 //        arrange
         var college = new College(null, "azad", "lar");
-        var professor = new Professor(null,"ali","b", "a", college);
+        var professor = new Professor(null, "ali", "125", "123", "dd", "a", college);
 //        act
         collegeRepository.save(college);
         professorRepository.save(professor);
-        professorRepository.update(new Professor(professor.getId(),"majid","d","a",college));
-        var load = professorRepository.findById(Professor.class,professor.getId());
+        professorRepository.update(new Professor(professor.getId(), "majid", "d", "12", "d", "a", college));
+        var load = professorRepository.findById(Professor.class, professor.getId());
 //        assert
         assertAll(
-                ()-> Assertions.assertThat("majid").isEqualTo(load.getName()) ,
-                ()-> Assertions.assertThat("d").isEqualTo(load.getDegree())
+                () -> Assertions.assertThat("majid").isEqualTo(load.getName()),
+                () -> Assertions.assertThat("d").isEqualTo(load.getDegree())
         );
     }
 
     @Test
-    void delete(){
+    void delete() {
 //        arrange
         var college = new College(null, "azad", "lar");
-        var professor = new Professor(null,"ali","b", "a", college);
+        var professor = new Professor(null, "ali", "124", "123", "b", "a", college);
 //        act
         collegeRepository.save(college);
         professorRepository.save(professor);
         professorRepository.delete(professor);
-        var load = professorRepository.findById(Professor.class,professor.getId());
+        var load = professorRepository.findById(Professor.class, professor.getId());
 //        assert
         Assertions.assertThatObject(load).isNull();
     }
 
     @Test
-    void findById(){
+    void findById() {
 //        arrange
         var college = new College(null, "azad", "lar");
-        var professor = new Professor(null,"ali","b", "a", college);
+        var professor = new Professor(null, "ali", "123", "123", "b", "a", college);
 //        act
         collegeRepository.save(college);
         professorRepository.save(professor);
@@ -82,10 +82,10 @@ class ProfessorRepositoryImplTest {
     }
 
     @Test
-    void findAll(){
+    void findAll() {
 //        arrange
         var college = new College(null, "azad", "lar");
-        var professor = new Professor(null,"ali","b", "a", college);
+        var professor = new Professor(null, "ali", "12", "123", "b", "a", college);
 //        act
         var size = professorRepository.findAll(Professor.class);
         collegeRepository.save(college);
@@ -94,5 +94,36 @@ class ProfessorRepositoryImplTest {
 //        assert
         Assertions.assertThat(size)
                 .hasSize(professorRepository.findAll(Professor.class).size());
+    }
+
+    @Test
+    void login() {
+//        arrange
+        var college = new College(null, "azad", "lar");
+        var professor = new Professor(null, "ali", "12", "123", "b", "a", college);
+//        act
+        collegeRepository.save(college);
+        professorRepository.save(professor);
+        var login = professorRepository.login(Professor.class, professor.getUserName(), professor.getPassword());
+//        assert
+        assertAll(
+                () -> Assertions.assertThat(login).isNotNull(),
+                () -> Assertions.assertThat(login.getUserName()).isEqualTo(professor.getUserName()),
+                () -> Assertions.assertThat(login.getPassword()).isEqualTo(professor.getPassword())
+        );
+    }
+
+    @Test
+    void getProfessorCourses() {
+//        arrange
+        var sec = new ScoreRepositoryImpl();
+        var professor = new Professor(null, "ali", "12", "123", "b", "a", null);
+        var score = new Score(null,null, professor,null, 1, 15.0);
+//        act
+        professorRepository.save(professor);
+        sec.save(score);
+        var load = professorRepository.getProfessorCourse(professor.getId());
+//        assert
+        Assertions.assertThat(load).hasSize(1);
     }
 }
