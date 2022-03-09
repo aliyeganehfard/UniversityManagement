a71d010df6b09c006201cbc504a53e6f540e11b4
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-03-09 22:24:49 +0330

    add ScoreException class

---
+++ src/main/java/controller/exception/ScoreException.java
@@ -0,0 +1,14 @@
+package controller.exception;
+
+public class ScoreException extends RuntimeException{
+    public ScoreException() {
+    }
+
+    public ScoreException(String message) {
+        super(message);
+    }
+
+    public ScoreException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
