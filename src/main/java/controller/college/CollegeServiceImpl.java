944a64bf29933445e224e56df0f030aa3dabdc42
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-03-09 21:31:57 +0330

    add CollegeServiceImpl class

---
+++ src/main/java/controller/college/CollegeServiceImpl.java
@@ -0,0 +1,15 @@
+package controller.college;
+
+import controller.ServiceImpl;
+import model.entity.College;
+import model.repository.college.CollegeRepository;
+import model.repository.college.CollegeRepositoryImpl;
+
+public class CollegeServiceImpl extends ServiceImpl<CollegeRepositoryImpl, College, Integer> implements CollegeRepository {
+    private CollegeRepositoryImpl collegeRepository;
+
+    public CollegeServiceImpl() {
+        super(new CollegeRepositoryImpl());
+        collegeRepository = new CollegeRepositoryImpl();
+    }
+}
