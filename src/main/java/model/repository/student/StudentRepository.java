c7c8b293be48deb121b3368e9b4002ae03d1cdb5
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-03-10 00:53:37 +0330

    fix add StudentRepository interface

--- src/main/java/model/repository/student/StudentRepository.java
+++ src/main/java/model/repository/student/StudentRepository.java
@@ -1,9 +1,11 @@
 package model.repository.student;
 
 import model.entity.Score;
+import model.entity.Student;
+import model.repository.LoginBase;
 
 import java.util.List;
 
-public interface StudentRepository {
+public interface StudentRepository extends LoginBase<Student> {
     List<Score> getStudentCourse(Integer id);
 }
