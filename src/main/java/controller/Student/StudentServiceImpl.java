80354dca2df62b1f2db13f372f7bbbdecfcde7d4
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-03-09 21:33:51 +0330

    add StudentServiceImpl class

---
+++ src/main/java/controller/Student/StudentServiceImpl.java
@@ -0,0 +1,22 @@
+package controller.Student;
+
+import controller.ServiceImpl;
+import model.entity.Score;
+import model.entity.Student;
+import model.repository.student.StudentRepositoryImpl;
+
+import java.util.List;
+
+public class StudentServiceImpl extends ServiceImpl<StudentRepositoryImpl, Student, Integer> implements StudentService {
+    private StudentRepositoryImpl studentRepository;
+
+    public StudentServiceImpl() {
+        super(new StudentRepositoryImpl());
+        this.studentRepository = new StudentRepositoryImpl();
+    }
+
+    @Override
+    public List<Score> getStudentCourse(Integer id) {
+        return studentRepository.getStudentCourse(id);
+    }
+}
