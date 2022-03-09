b365d47f5fda9154b77fcac2e1df8b13b80fd97c
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-03-09 21:32:56 +0330

    add ProfessorServiceImpl class

---
+++ src/main/java/controller/Proffessor/ProfessorServiceImpl.java
@@ -0,0 +1,22 @@
+package controller.Proffessor;
+
+import controller.ServiceImpl;
+import model.entity.Professor;
+import model.entity.Score;
+import model.repository.professor.ProfessorRepositoryImpl;
+
+import java.util.List;
+
+public class ProfessorServiceImpl extends ServiceImpl<ProfessorRepositoryImpl, Professor,Integer> implements ProfessorService {
+    private ProfessorRepositoryImpl professorRepository;
+
+    public ProfessorServiceImpl() {
+        super(new ProfessorRepositoryImpl());
+        professorRepository = new ProfessorRepositoryImpl();
+    }
+
+    @Override
+    public List<Score> getProfessorCourse(Integer id) {
+        return professorRepository.getProfessorCourse(id);
+    }
+}
