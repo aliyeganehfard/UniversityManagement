1cd2a0c1efa7c31d2b897763b7521b3cbf1fceed
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-03-09 20:52:17 +0330

    add CourseRepositoryImpl class

---
+++ src/main/java/model/repository/course/CourseRepositoryImpl.java
@@ -0,0 +1,7 @@
+package model.repository.course;
+
+import model.entity.Course;
+import model.repository.RepositoryImpl;
+
+public class CourseRepositoryImpl extends RepositoryImpl<Course,Integer> implements CourseRepository{
+}
